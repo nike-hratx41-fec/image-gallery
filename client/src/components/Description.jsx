f79b21112a0ed436242699525e957ec4ad4b2616
friedpies <kenmarut@gmail.com>
2019-05-18 10:44:17 -0500

    deploy practice 2

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import Axios from "axios";
 
 class Description extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      sku: "CJ0066-900",
       description: ""
     };
   }
 
   componentDidMount() {
-    console.log("MOUNTED");
+    Axios.get(`http://localhost:3001/api/description/${this.state.sku}`)
+      .then(description => {
+        this.setState({ description: description.data });
+      })
+      .catch(err => {});
   }
 
   render() {
-    return <div>THIS IS THE DESCRIPTION</div>;
+    return <div>{this.state.description}</div>;
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ const port = 3001;
 
 app.use(express.static(__dirname + "/../client/dist"));
 
-app.get("/api/description/:sku", (req, res) => {
+app.get("/api/:info/:sku", (req, res) => {
   const sku = req.params.sku;
+  const info = req.params.info;
+  console.log(description);
   Shoe.findOne({ sku })
     .exec((err, shoe) => {
-      console.log(shoe);
+      res.json(shoe[info]);
     })
     .catch(err => {
       console.log("there was an error finding the item");
